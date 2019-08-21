c23b87ade284c0af9c0bdbfcd9c04945e10d5733
Pierre TROLLE <pierre.trolle@manomano.com>
2019-08-21 21:48:20 +0200

    feat(npm-scripts): add start:fr and start:en

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -1,19 +1,14 @@
 /* eslint-disable no-param-reassign */
-
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
-// TODO extract lang + check config
-
-console.log('process.env.LANGUAGE')
-console.log(process.env.LANGUAGE)
 export default ({ app, store }) => {
   // Set i18n instance on app
   // This way we can use it in middleware and pages asyncData/fetch
   app.i18n = new VueI18n({
-    locale: process.env.LANGUAGE,
+    locale: store.state.locale,
     fallbackLocale: 'fr',
     messages: {
       'en': require('~/locales/en.json'),
